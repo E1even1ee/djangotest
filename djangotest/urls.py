12450603b0e414de6b302c89117d086a43204fd2
E1evn1ee <752944885@qq.com>
2018-05-28 15:15:52 +1000

    Online Document Preview

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 urlpatterns = [
-    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    url(r'^polls/', include('polls.urls')), #this line added
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
